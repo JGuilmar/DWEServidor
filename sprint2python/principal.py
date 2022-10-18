4f901396a905f6d892303e8c3d9eaefafb8c776b
Jose Bayona <jbayonac@fpcoruna.afundacion.org>
2022-10-19 00:09:09 +0200

    Ejercicio 6, modificado principal.py para que nos muestre el tiempo en ejecución que demora en realizase cada una de las funciones de factorial.

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,7 +1,8 @@
 from factorial import func_factorial
 from factorial2 import func_factorial2
+import time
 
-print('\n¡¡¡ NÚMERO FACTORIAL !!!')
+print('\n\n¡¡¡ NÚMERO FACTORIAL !!!')
 print('\nFormas para hallar el fastorial de un numero...')
 print('1. Sin recursividad... ')
 print('2. Con recursividad... ')
@@ -11,12 +12,20 @@ opcion = input('\nTeclea una opción: ')
 while(opcion != '1') & (opcion != '2'):
     opcion = input('\nTeclea una opción válida (1 o 2): ')
 
-numero = int(input('Teclea el número al que se calculará su factorial: '))
+numero = float(input('\nTeclea el número al que se calculará su factorial: '))
 
 if(numero < 0):
-    print('ERROR!!! No se puede hallar el factorial de un número negativo. ')
+    print('\nERROR!!! No se puede hallar el factorial de un número negativo. ')
 else:
     if(opcion == '1'):
-        print('El factorial de ' + str(numero) +' sin recursividad es: ' + str(func_factorial2(numero)))
+        star_time = time.time()
+        print('\nEl factorial de ' + str(numero) +' sin recursividad es: ' + str(func_factorial2(numero)))
+        end_time = time.time()
+        elapsed_time=end_time-star_time
+        print('\nEl tiempo de ejecución de la forma sin recursividad es: ' + str(elapsed_time) + ' s')
     else:
-        print('El factorial de ' + str(numero) +' con recursividad es: ' + str(func_factorial(numero)))
+        star_time = time.time()
+        print('\nEl factorial de ' + str(numero) +' con recursividad es: ' + str(func_factorial(numero)))
+        end_time = time.time()
+        elapsed_time=end_time-star_time
+        print('\nEl tiempo de ejecución de la forma con recursividad es: ' + str(elapsed_time) + ' s')
