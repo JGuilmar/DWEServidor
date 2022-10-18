695298547d4a383a3bd2c126ef296b8361293718
Jose Bayona <jbayonac@fpcoruna.afundacion.org>
2022-10-19 00:25:27 +0200

    Ejercicio 7, modificado concurso.py para que imprima 2 de las 3 preguntas aleatoriamente y hallar la puntuación obtenia.

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,3 +1,4 @@
+import random
 #Función para obtener la puntuación de las preguntas 
 def puntuacion(rpt_correcta, puntaje_acumulado):
     rpt_usuario = input('\nTeclea la respuesta correcta: ').lower()
@@ -13,38 +14,50 @@ def puntuacion(rpt_correcta, puntaje_acumulado):
         print('-5 puntos')
     return puntaje_acumulado   
    
+#Array con el número de preguntas
+p = [1, 2, 3]
+#Desordenamos el array de manera aleatoria
+random.shuffle(p)
 
-puntaje_inicial = 0
 
 print("¡¡¡ RESPONDE LAS PREGUNTAS Y CONSIGUE LA MÁXIMA PUNTUACIÓN !!!")
 print("RESPUESTA CORRECTA (+10) \nRESPUESTA INCORRECTA (-5)")
+#Mostramos de nuevo el array
+print(p)
 
-print("\n\nPregunta 1:")
-print("¿Qué es Python? ")
-print("a. Es un lenguaje de programación de bajo nivel.")
-print("b. Es un lenguaje de preprocesado de hipertexto.")
-print("c. Es un lenguaje de programación interpretado dinámicamente tipado.")
+puntaje_final = 0
+#Elegimos los dos primeros numero del array desordenado
+if p[0] == 1 or p[1] == 1:
+    print("\n\nPregunta 1:")
+    print("¿Qué es Python? ")
+    print("a. Es un lenguaje de programación de bajo nivel.")
+    print("b. Es un lenguaje de preprocesado de hipertexto.")
+    print("c. Es un lenguaje de programación interpretado dinámicamente tipado.")
 
-rptCorrecta1 = 'c'
-puntaje_final = puntuacion (rptCorrecta1,puntaje_inicial)
+    rptCorrecta1 = 'c'
+    puntaje_final = puntuacion (rptCorrecta1,puntaje_final)
 
-print("\n\nPregunta 2:")
-print("¿Qué es __init__?")
-print("a. Es una variable para inicializar los atributos de un objeto.")
-print("b. Es un método o constructor especial de una clase en Python.")
-print("c. Es el último método que se ejecuta cuando se crea un objeto.")
 
-rptCorrecta2 = 'b'
-puntaje_final = puntuacion (rptCorrecta2,puntaje_final)
+if p[0] == 2 or p[1] == 2:
+    print("\n\nPregunta 2:")
+    print("¿Qué es __init__?")
+    print("a. Es una variable para inicializar los atributos de un objeto.")
+    print("b. Es un método o constructor especial de una clase en Python.")
+    print("c. Es el último método que se ejecuta cuando se crea un objeto.")
 
-print("\n\nPregunta 3:")
-print("¿Cómo se escriben los comentarios en Python?")
-print("a. ##Los comentarios en Python empiezan así")
-print("b. //Los comentarios en Python empiezan así")
-print("c. #Los comentarios en Python empiezan así")
+    rptCorrecta2 = 'b'
+    puntaje_final = puntuacion (rptCorrecta2,puntaje_final)
 
-rptCorrecta3 = 'c'
-puntaje_final = puntuacion (rptCorrecta3,puntaje_final)
+
+if p[0] == 3 or p[1] == 3:
+    print("\n\nPregunta 3:")
+    print("¿Cómo se escriben los comentarios en Python?")
+    print("a. ##Los comentarios en Python empiezan así")
+    print("b. //Los comentarios en Python empiezan así")
+    print("c. #Los comentarios en Python empiezan así")
+
+    rptCorrecta3 = 'c'
+    puntaje_final = puntuacion (rptCorrecta3,puntaje_final)
 
 print("\n\nPUNTUACIÓN OBTENIDA: " + str(puntaje_final) + ' puntos')
 
