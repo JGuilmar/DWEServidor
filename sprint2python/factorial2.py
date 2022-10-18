e15ef6f3d304e0acb330a48df8305da4dbbf4316
Jose Bayona <jbayonac@fpcoruna.afundacion.org>
2022-10-19 00:06:39 +0200

    Ejercicio 5, modificado factorial2.py para poder ser llamado junto a factorial.py desde principal.py.Modificamos también principal.py.

--- sprint2python/factorial2.py
+++ sprint2python/factorial2.py
@@ -1,4 +1,3 @@
-print('¡¡¡ NÚMERO FACTORIAL SIN RECURSIVIDAD!!!')
 def func_factorial2(numero):
     if(numero == 0 or numero ==1):
         return 1
@@ -8,12 +7,5 @@ def func_factorial2(numero):
             fact *= numero
             numero -= 1
         return fact
-    
-
-numero = int(input('Teclea el número al que se calculará su factorial: '))
-if(numero < 0):
-    print('No se puede sacar el factorial de un numero negativo')
-else:
-    print('El factorial de ' + str(numero) + ' es: ' + str(func_factorial2(numero)))
 
 
--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,9 +1,22 @@
 from factorial import func_factorial
+from factorial2 import func_factorial2
 
-print('¡¡¡ NÚMERO FACTORIAL CON RECURSIVIDAD !!!')
+print('\n¡¡¡ NÚMERO FACTORIAL !!!')
+print('\nFormas para hallar el fastorial de un numero...')
+print('1. Sin recursividad... ')
+print('2. Con recursividad... ')
+
+opcion = input('\nTeclea una opción: ')
+
+while(opcion != '1') & (opcion != '2'):
+    opcion = input('\nTeclea una opción válida (1 o 2): ')
+
 numero = int(input('Teclea el número al que se calculará su factorial: '))
 
 if(numero < 0):
-    print('No se puede sacar el factorial de un numero negativo')
+    print('ERROR!!! No se puede hallar el factorial de un número negativo. ')
 else:
-    print('El factorial de ' + str(numero) +' es: ' + str(func_factorial(numero)))
+    if(opcion == '1'):
+        print('El factorial de ' + str(numero) +' sin recursividad es: ' + str(func_factorial2(numero)))
+    else:
+        print('El factorial de ' + str(numero) +' con recursividad es: ' + str(func_factorial(numero)))
