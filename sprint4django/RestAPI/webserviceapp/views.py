0260f1b0e171cccdf018905d3dd83c8b414a947a
JGuilmar <jbayonac@fpcoruna.afundacion.org>
2022-11-08 15:19:55 +0100

    Creando un APIREST con Django
    Creamos la app webserviceapp y la añadimos al archvo settings.py ubicado en /RestAPI/RestAPI
    Definimos las clases Python de las tablas de nuestra base de datos en models.py.
    En settings.py cambiamos la DATABASES que vine por defecto por los valores de nuestra base de datos 'mysitedb' y la importamos modificando el achivo __init__ ubicada en la misma ruta que settings.py.
    Verificamos si se importaron las clases en models.py ubicado en la ruta /RestAPI/webserviceapp/migrations/models.py.
    Escrimimos nuestras pàginas web a travès de funciones en views.py ubicado en la ruta /RestAPI/RestAPI/webserviceapp/migrations.
    Y las mapeamos en urls.py de la ruta /RestAPI/RestAPI.
    
    Implementación del primer endPoint GET/canciones.

---
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from .models import Tcanciones
+
+def pagina_de_prueba(request):
+	return HttpResponse("<h1>Hola caracola</h1>");
+
+def devolver_canciones(request):
+	lista = Tcanciones.objects.all()
+	respuesta_final = []
+	for fila_sql in lista:
+		diccionario = {}
+		diccionario['id'] = fila_sql.id
+		diccionario['nombre'] = fila_sql.nombre
+		diccionario['interprete'] = fila_sql.interprete
+		diccionario['genero'] = fila_sql.genero
+		respuesta_final.append(diccionario)
+	return JsonResponse(respuesta_final, safe=False)
+
+
+
+# Create your views here.
