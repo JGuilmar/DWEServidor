32c97d866200836415a78b12e60014e9f8526e28
JGuilmar <jbayonac@fpcoruna.afundacion.org>
2022-11-14 13:49:16 +0100

    Creaciòn de un APIREST con Django
    Creado el endpoint GET/canciones/<id> con el que obtendremos un json con la lista de comentarios a través del id de una cancion.

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-from .models import Tcanciones
+from .models import Tcanciones, Tcomentarios, Tusuarios
+import json
+
 
 def pagina_de_prueba(request):
 	return HttpResponse("<h1>Hola caracola</h1>");
@@ -19,6 +21,24 @@ def devolver_canciones(request):
 		respuesta_final.append(diccionario)
 	return JsonResponse(respuesta_final, safe=False)
 
+def devolver_cancion_por_id(request, id_solicitado):
+	cancion = Tcanciones.objects.get(id = id_solicitado)
+	comentarios = cancion.tcomentarios_set.all()
+	lista_comentarios = []
+	for fila_comentario_sql in comentarios:
+		diccionario = {}
+		diccionario['id'] = fila_comentario_sql.id
+		diccionario['comentario'] = fila_comentario_sql.comentario
+		lista_comentarios.append(diccionario)
+	resultado = {
+		'id' : cancion.id,
+		'nombre' : cancion.nombre,
+		'url_imagen' : cancion.url_imagen,
+		'interprete' : cancion.interprete,
+		'genero' : cancion.genero,
+		'comentarios' : lista_comentarios
+	}
+	return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
 
 
 # Create your views here.
